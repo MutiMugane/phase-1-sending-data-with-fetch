e53d13c8caa9a56a79a4d6fdca3c025ebdd342a7
+ <+>
2022-09-04 23:22:55 +0300

    Commit One

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Add your code here
+function submitData(){
+  fetch("http://localhost:3000/users"), {
+    method:"POST",
+   
+        headers:{
+        "Content-Type": 'application/json',
+        Accept: 'application/json',
+        },
+        
+        body: JSON.stringify({
+            "name": "Steve",
+            "Email": "steve@steve.com",
+           }),
+    
+        }
+    
+        }
+        response.json().then(d => d.json())
+        response.json().then(resp => {
+        console.log(resp),{
+        
+        // response.json().then(data => {
+        //     console.log(data);  });
+        catch(err){
+            console.log(err);
+        }
+        
+        }
+        })
+        
+    
