672efd66f6d358998d1162e50f5a37c7e1cfa592
+ <+>
2022-09-08 11:23:04 +0300

    Second Commit

--- index.js
+++ index.js
@@ -1,31 +1,55 @@
-function submitData(){
-  fetch("http://localhost:3000/users"), {
-    method:"POST",
+  // fetch("http://localhost:3000/users"), {
+  //   method:"POST",
    
-        headers:{
-        "Content-Type": 'application/json',
-        Accept: 'application/json',
-        },
+  //       headers:{
+  //       "Content-Type": 'application/json',
+  //       Accept: 'application/json',
+  //       },
         
-        body: JSON.stringify({
-            "name": "Steve",
-            "Email": "steve@steve.com",
-           }),
+  //       body: JSON.stringify({
+  //           "name": "Steve",
+  //           "Email": "steve@steve.com",
+  //          }),
     
-        }
+  //       }
     
-        }
-        response.json().then(d => d.json())
-        response.json().then(resp => {
-        console.log(resp),{
+  //       }
+  //       response.json().then(d => d.json())
+  //       response.json().then(resp => {
+  //       console.log(resp),{
         
-        // response.json().then(data => {
-        //     console.log(data);  });
-        catch(err){
-            console.log(err);
-        }
+  //       // response.json().then(data => {
+  //       //     console.log(data);  });
+  //       catch(err){
+  //           console.log(err);
+  //       }
         
-        }
-        })
+  //       }
+  //       })
         
+  function submitData(name,Email){
+    const formData = {
+       name: "Byron",
+       email: "Poodle",
+     };
+    
+    const configurationObject = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify(formData),
+    };
+    
+    fetch("http://localhost:3000/users", configurationObject)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (object) {
+        console.log(object);
+      });
+      
+  };
+  
     
